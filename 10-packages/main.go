b40b5deadd6a49651246eed0111f2e6e92f1e1f8
Vladimir Egorov <vegorov@gmail.com>
2023-05-20 21:35:32 +0300

    comment in 10-packages re VS Code

--- 10-packages/main.go
+++ 10-packages/main.go
@@ -5,7 +5,12 @@ import (
 	// VS Code будет жаловаться, что не может найти пакет <module-name>/store,
 	// если открыть не текущую директорию, как корневую, а уровнем выше (то есть весь репозиторий)
 	// https://stackoverflow.com/questions/58518588/vscode-could-not-import-golang-package
-	// Для того, чтобы обойти, можно использовать workspaces - go 1.18+
+	// Для того, чтобы обойти, можно использовать workspaces - go 1.18+ (пока не удалось понять как это
+	// должно работать).
+	// Проблема не проявляется, если открыть в VS Code директорию 10-packages, то есть не открывать
+	// 10-packages как часть репозитория. Если в VS Code открыта корневая директория репозитория, то
+	// будем видеть во вкладке Problems вот такое:
+	// could not import packages-test/store (cannot find package "packages-test/store" in any of ....
 	"packages-test/store"
 )
 
